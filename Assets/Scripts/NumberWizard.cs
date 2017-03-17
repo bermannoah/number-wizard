5a7ae9492300a6783fb989556622a4c27cda852f
Noah Berman <noah@noahberman.org>
2017-03-16 22:48:37 -0600

    adds scoping methods

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
+	int max = 1000;
+	int min = 1;
+	int guess = 500;
+
 	// Use this for initialization
 	void Start () {
+		max = max + 1;
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head, but don't tell me.");
-
-		int max = 1000;
-		int min = 1;
 
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
 
-		print ("Is the number higher or lower than 500?");
+		print ("Is the number higher or lower than " + guess + "?");
 		print ("Up arrow for higher, down for lower, enter for equals.");
 	}
 	
@@ -23,9 +25,15 @@ public class NumberWizard : MonoBehaviour {
 	void Update ()
 	{
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			print ("Up arrow pressed");
+			// print ("Up arrow pressed");
+			min = guess;
+			guess = (max + min) / 2;
+			print ("Is the number higher or lower than " + guess + "?");
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			print ("Down arrow pressed");
+			// print ("Down arrow pressed");
+			max = guess;
+			guess = (max + min) / 2;
+			print ("Is the number higher or lower than " + guess + "?");
 		} else if (Input.GetKeyDown (KeyCode.Return)) {
 			print ("I won!");
 		}
